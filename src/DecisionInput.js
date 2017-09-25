7dc4988c491671c1409b20094cf6d7796c60368b
nadinesk <nadine.khattak@gmail.com>
2017-09-25 18:47:51 -0400

    styling

--- src/DecisionInput.js
+++ src/DecisionInput.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ShowPC from './ShowPC'
+import { Button, ButtonToolbar, Grid, Row, Col, Clearfix, FormGroup, FormControl, ControlLabel } from 'react-bootstrap'
 
 export default class DecisionInput extends Component {
 
@@ -34,7 +35,7 @@ export default class DecisionInput extends Component {
    
     this.handleChange = this.handleChange.bind(this)
     
-    this.handleSubmit = this.handleSubmit.bind(this)
+    this.clearForm = this.clearForm.bind(this)
   }
 
   handleChange(event) {
@@ -48,9 +49,32 @@ export default class DecisionInput extends Component {
 
   }
 
-  handleSubmit(event) {
+  clearForm() {
 	
-	console.log(this.state)
+	 this.setState({
+    		question: '',
+	      	prodescription1: '', 
+	      	proweight1: 0,
+	        prodescription2: '', 
+	      	proweight2: 0,
+	      	prodescription3: '', 
+	      	proweight3: 0,
+	      	prodescription4: '', 
+	      	proweight4: 0,
+	      	prodescription5: '', 
+	      	proweight5: 0,
+	      	condescription1: '', 
+	      	conweight1: 0,
+	        condescription2: '', 
+	      	conweight2: 0,
+	      	condescription3: '', 
+	      	conweight3: 0,
+	        condescription4: '', 
+	      	conweight4: 0,
+	      	condescription5: '', 
+	      	conweight5: 0,
+	    });
+
 
   }
 
@@ -62,133 +86,151 @@ export default class DecisionInput extends Component {
   
     
     return (
-     <div>
-	     <form onSubmit={this.handleSubmit}>
-	        <div>
-	        	<label>
-	        		<h2>My Decision</h2> 
-	        			<input type="text" name="question" onChange={this.handleChange} />
-	        	</label>
-		        <label>	  
-		          <h3>Pros</h3>       		          
-		        </label>
-		        <div>
-		        	<div>
-				        <label>	 
-				        description        
-				          <input type="text" name="prodescription1" onChange={this.handleChange} />
-				        </label>
-				        <label>	    
-				        weight     
-				          <input type="number" name="proweight1" onChange={this.handleChange} />
-				        </label>
-			        </div>
-			        <div>
-			        	<label>	 
-				        description        
-				          <input type="text" name="prodescription2" onChange={this.handleChange} />
-				        </label>
-				        <label>	    
-				        weight     
-				          <input type="number" name="proweight2" onChange={this.handleChange} />
-				        </label>
-			        </div>
-			        <div>
-				        <label>	 
-				        description        
-				          <input type="text" name="prodescription3" onChange={this.handleChange} />
-				        </label>
-				        <label>	    
-				        weight     
-				          <input type="number" name="proweight3" onChange={this.handleChange} />
-				        </label>
-			        </div>
-			        <div>
-			        	<label>	 
-				        description        
-				          <input type="text" name="prodescription4" onChange={this.handleChange} />
-				        </label>
-				        <label>	    
-				        weight     
-				          <input type="number" name="proweight4" onChange={this.handleChange} />
-				        </label>
-			        </div>
-			        <div>
-			        	<label>	 
-				        description        
-				          <input type="text" name="prodescription5" onChange={this.handleChange} />
-				        </label>
-				        <label>	    
-				        weight     
-				          <input type="number" name="proweight5" onChange={this.handleChange} />
-				        </label>
-			        </div>
-		        </div>
-	        </div>
-	        <div>
-		       <label>	  
-		          <h3>Cons</h3>       		          
-		        </label>
-		        <div>
-		        	<div>
-				        <label>	 
-				        description        
-				          <input type="text" name="condescription1" onChange={this.handleChange} />
-				        </label>
-				        <label>	    
-				        weight     
-				          <input type="number" name="conweight1" onChange={this.handleChange} />
-				        </label>
-			        </div>
-			        <div>
-			        	<label>	 
-				        description        
-				          <input type="text" name="condescription2" onChange={this.handleChange} />
-				        </label>
-				        <label>	    
-				        weight     
-				          <input type="number" name="conweight2" onChange={this.handleChange} />
-				        </label>
-			        </div>
-			        <div>
-				        <label>	 
-				        description        
-				          <input type="text" name="condescription3" onChange={this.handleChange} />
-				        </label>
-				        <label>	    
-				        weight     
-				          <input type="number" name="conweight3" onChange={this.handleChange} />
-				        </label>
-			        </div>
-			        <div>
-			        	<label>	 
-				        description        
-				          <input type="text" name="condescription4" onChange={this.handleChange} />
-				        </label>
-				        <label>	    
-				        weight     
-				          <input type="number" name="conweight4" onChange={this.handleChange} />
-				        </label>
-			        </div>
-			         <div>
-			        	<label>	 
-				        description        
-				          <input type="text" name="condescription5" onChange={this.handleChange} />
-				        </label>
-				        <label>	    
-				        weight     
-				          <input type="number" name="conweight5" onChange={this.handleChange} />
-				        </label>
-			        </div>
-		        </div>
-		     </div>
-	        <input type="submit" value="Clear" />
-	      </form>
-
-	      <ShowPC items={this.state} /> 
+     
+    	<Grid>
+    	    <Row className="show-grid">
+    	      <Col md={12} > 
+			   	<label>
+			    	<h2>My Decision</h2> 
+			        	<input type="text" name="question" onChange={this.handleChange} />
+				</label>
+			   </Col> 
+			</Row> 
+			<Row className="show-grid">      	
+				<Col md={6} >
+					        <div>
+					          <h3>Pros</h3>       		          
+					        </div>
+					        	<div>
+					        	<strong> 1. </strong>
+								</div> 				        
+					        
+				                <label>	 
+						        description        
+						          <input type="text" name="prodescription1" onChange={this.handleChange} />
+						        </label>
+						        <label>	    
+						        weight     
+						          <input type="number" name="proweight1" onChange={this.handleChange} />
+						        </label>
+						        <div>
+					        	<strong> 2. </strong>
+								</div> 
+					        	<label>	 
+						        description        
+						          <input type="text" name="prodescription2" onChange={this.handleChange} />
+						        </label>
+						        <label>	    
+						        weight     
+						          <input type="number" name="proweight2" onChange={this.handleChange} />
+						        </label>
+						        <div>
+					        	<strong> 3. </strong>
+								</div> 
+					            <label>	 
+						        description        
+						          <input type="text" name="prodescription3" onChange={this.handleChange} />
+						        </label>
+						        <label>	    
+						        weight     
+						          <input type="number" name="proweight3" onChange={this.handleChange} />
+						        </label>
+					        	<div>
+					        	<strong> 4. </strong>
+								</div> 
+					        	<label>	 
+						        description        
+						          <input type="text" name="prodescription4" onChange={this.handleChange} />
+						        </label>
+						        <label>	    
+						        weight     
+						          <input type="number" name="proweight4" onChange={this.handleChange} />
+						        </label>
+					      		<div>
+					        	<strong> 5. </strong>
+								</div> 
+					        	<label>	 
+						        description        
+						          <input type="text" name="prodescription5" onChange={this.handleChange} />
+						        </label>
+						        <label>	    
+						        weight     
+						          <input type="number" name="proweight5" onChange={this.handleChange} />
+						        </label>
+					       
+			       </Col>
+			       <Col md={6} >
+			        
+				       <div>	  
+				          <h3>Cons</h3>       		          
+				        </div>
+				       			<div>
+					        	<strong> 1. </strong>
+								</div> 
+						        <label>	 
+						        description        
+						          <input type="text" name="condescription1" onChange={this.handleChange} />
+						        </label>
+						        <label>	    
+						        weight     
+						          <input type="number" name="conweight1" onChange={this.handleChange} />
+						        </label>
+					       		<div>
+					        	<strong> 2. </strong>
+								</div> 
+					        	<label>	 
+						        description        
+						          <input type="text" name="condescription2" onChange={this.handleChange} />
+						        </label>
+						        <label>	    
+						        weight     
+						          <input type="number" name="conweight2" onChange={this.handleChange} />
+						        </label>
+					       		<div>
+					        	<strong> 3. </strong>
+								</div> 
+						        <label>	 
+						        description        
+						          <input type="text" name="condescription3" onChange={this.handleChange} />
+						        </label>
+						        <label>	    
+						        weight     
+						          <input type="number" name="conweight3" onChange={this.handleChange} />
+						        </label>
+					        	<div>
+					        	<strong> 4. </strong>
+								</div> 
+					        	<label>	 
+						        description        
+						          <input type="text" name="condescription4" onChange={this.handleChange} />
+						        </label>
+						        <label>	    
+						        weight     
+						          <input type="number" name="conweight4" onChange={this.handleChange} />
+						        </label>
+					       		<div>
+					        	<strong> 5. </strong>
+								</div> 
+					        	<label>	 
+						        description        
+						          <input type="text" name="condescription5" onChange={this.handleChange} />
+						        </label>
+						        <label>	    
+						        weight     
+						          <input type="number" name="conweight5" onChange={this.handleChange} />
+						        </label>
+						        <div>
+					        
+			        				<input type="submit" value="Clear" onClick={this.clearForm} />
+			        			</div> 
+			      </Col> 
+			     </Row> 
+			      <ShowPC items={this.state} /> 
+			     
+			   </Grid>
 
 	  
-	  </div> 
+	  
     );
   }
 }
--- src/ShowPC.js
+++ src/ShowPC.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button, ButtonToolbar, Grid, Row, Col, Clearfix, FormGroup, FormControl, ControlLabel, Table } from 'react-bootstrap'
 
 export default class DecisionInput extends Component {
 
@@ -20,44 +21,112 @@ export default class DecisionInput extends Component {
   					parseFloat(this.props.items.conweight5)
 
     return (
-     <div><br />
-     	<div>The Question is: {this.props.items.question} </div>
-     	<div>
-	     	<h3>Pros </h3>
-	     		<p> {this.props.items.prodescription1} </p> 
-	     		<p>{this.props.items.proweight1}</p>
-	     		<p> {this.props.items.prodescription2} </p> 
-	     		<p>{this.props.items.proweight2}</p>
-	     		<p> {this.props.items.prodescription3} </p> 
-	     		<p>{this.props.items.proweight3}</p>
-	     		<p> {this.props.items.prodescription4} </p> 
-	     		<p>{this.props.items.proweight4}</p>
-	     		<p> {this.props.items.prodescription5} </p> 
-	     		<p>{this.props.items.proweight5}</p>
-	     		<p>{total_pro}</p>
-	     </div>
+     <Grid>
+     	<br />
+     	<Row>
+     		<Col md={12}>
+     			The Question is: {this.props.items.question} ?
+     		</Col> 
+     	</Row>
+     	<Row>
+     	<Col md={6}>
+     	<h3>Pros</h3>  
+     	<Table striped bordered condensed hover>
+     		
+     		<thead>
+     			<tr>
+     				<th>Item</th> 
+     				<th>Weight</th> 
+     			</tr> 
+     		</thead> 
+     		<tbody>
+     			
+     				<tr>
+     					<td>{this.props.items.prodescription1}</td> 
+     					<td>{this.props.items.proweight1}</td> 
+     				</tr> 
+     				<tr>
+     					<td>{this.props.items.prodescription2}</td> 
+     					<td>{this.props.items.proweight2}</td> 
+     				</tr> 
+     				<tr>
+     					<td>{this.props.items.prodescription3}</td> 
+     					<td>{this.props.items.proweight4}</td> 
+     				</tr> 
+     				<tr>
+     					<td>{this.props.items.prodescription4}</td> 
+     					<td>{this.props.items.proweight4}</td> 
+     				</tr> 
+     				<tr>
+     					<td>{this.props.items.prodescription5}</td> 
+     					<td>{this.props.items.proweight5}</td> 
+     				</tr> 
+     				
+     				
+     			
+     			
+     		</tbody>
+     	</Table>
+     	</Col> 
+     	<Col md={6}>
+     	<h3> Cons </h3> 
+     		
+		<Table striped bordered condensed hover>
+     		
+     		<thead>
+     			<tr>
+     				<th>Item</th> 
+     				<th>Weight</th> 
+     			</tr> 
+     		</thead> 
+     		<tbody>
+     			
+     				<tr>
+     					<td>{this.props.items.condescription1}</td> 
+     					<td>{this.props.items.conweight1}</td> 
+     				</tr> 
+     				<tr>
+     					<td>{this.props.items.condescription2}</td> 
+     					<td>{this.props.items.conweight2}</td> 
+     				</tr> 
+     				<tr>
+     					<td>{this.props.items.condescription3}</td> 
+     					<td>{this.props.items.conweight4}</td> 
+     				</tr> 
+     				<tr>
+     					<td>{this.props.items.condescription4}</td> 
+     					<td>{this.props.items.conweight4}</td> 
+     				</tr> 
+     				<tr>
+     					<td>{this.props.items.condescription5}</td> 
+     					<td>{this.props.items.conweight5}</td> 
+     				</tr> 
+     				
+     				
+     			
+     			
+     		</tbody>
+     	</Table>
+     	</Col> 
+	     </Row>
 
-		<div>
-	     	<h3>Pros </h3>
-	     		<p> {this.props.items.condescription1} </p> 
-	     		<p>{this.props.items.conweight1}</p>
-	     		<p> {this.props.items.condescription2} </p> 
-	     		<p>{this.props.items.conweight2}</p>
-	     		<p> {this.props.items.condescription3} </p> 
-	     		<p>{this.props.items.conweight3}</p>
-	     		<p> {this.props.items.condescription4} </p> 
-	     		<p>{this.props.items.conweight4}</p>
-	     		<p> {this.props.items.condescription5} </p> 
-	     		<p>{this.props.items.conweight5}</p>
-	     		<p>{total_con}</p>
-	     </div>
+	     <Row> 
+		     <Col md={12}> 
+		     	<h4>Total: {total_pro - total_con}</h4>
+		     </Col> 
+	     </Row> 
+	     <Row>
+	     	<Col md={12}> 
+		     	<h4>To Do, or Not To Do: 
+		     	{total_pro - total_con === 0 ? "TBD" : total_pro - total_con > 0 ? "You should do it" : "You should not do it"}
+		     	</h4> 
+	     	</Col> 
+	     </Row>
 
-	     <div>Total</div>
-	     <div>{total_pro - total_con}</div>
-	     <div>To Do, or Not To Do </div>	     
-	     {total_pro - total_con === 0 ? "TBD" : total_pro - total_con > 0 ? "You should do it" : "You should not do it"}
-     		 
-	  </div> 
+     	 <br /> 		 
+	  </Grid> 
+
+
     );
   }
 }
